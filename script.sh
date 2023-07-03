bb4c621dfc3b56b822941ec044bc77672672f116
Federico Mattiuzzi <79696658+JLarn@users.noreply.github.com>
2023-07-03 19:07:28 +0200

    forse lo script ora va

--- script.sh
+++ script.sh
@@ -1,16 +1,7 @@
 #! /usr/bin/bash
 
-sudo apt install mongo-tools
-if [ ! -d api ];
-    then
-        mkdir api
-        cd api/
-        git clone https://github.com/JLarn/API-onprem.git
-    else
-        cd api/
-        git pull            
-fi
+cd api/
 
 docker compose up
 
-mongoimport -d provisioning -c devices --jsonArray --file provisioning.devices.json
+mongoimport --uri mongodb://root:password123@0.0.0.0:27017 -d provisioning -c devices --jsonArray --file provisioning.devices.json
